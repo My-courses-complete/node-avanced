cea4356fdf35066fb700e5a7abbff818a67fd018
JMR-Mejia <chucho4720013@gmail.com>
2021-09-16 17:48:12 -0500

    deployment models with sequelize

---
+++ platziverse-db/lib/db.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+let sequelize = null
+
+module.exports = function setupDatabase (config) {
+  if (!sequelize) {
+    sequelize = new Sequelize(config)
+  }
+
+  return sequelize
+}
