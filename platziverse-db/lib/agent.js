0781a0f4e2964ea94362e4e6f3cbfdea5580124e
JMR-Mejia <chucho4720013@gmail.com>
2021-09-17 10:17:56 -0500

    Create fixtures and basic definition  Agentservice

---
+++ platziverse-db/lib/agent.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = function setupAgent (AgentModel) {
+  function findById (id) {
+
+  }
+
+  return {
+    findById
+  }
+}
--- platziverse-db/tests/agent.test.js
+++ platziverse-db/tests/agent.test.js
@@ -4,42 +4,58 @@ const test = require('ava')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire')
 
+const agentFixtures = require('./fixtures/agent')
+
 const config = {
   logging: function () {}
 }
 
-let MetricStub = {
-    belongsTo: sinon.spy()
+const MetricStub = {
+  belongsTo: sinon.spy()
 }
 
+const single = Object.assign({}, agentFixtures.single)
+const id = 1
 let AgentStub = null
 let db = null
 let sandbox = null
 
 test.beforeEach(async () => {
-    sandbox = sinon.createSandbox()
-    AgentStub = {
-        hasMany: sandbox.spy()
-    }
-    
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
   const setupDatabase = proxyquire('../', {
-      './models/agent': () => AgentStub,
-      './models/metric': () => MetricStub,
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
   })
   db = await setupDatabase(config)
 })
 
-test.afterEach(t => {
-    sandbox && sinon.restore()
+test.afterEach((t) => {
+  sandbox && sinon.restore()
 })
 
-test('make it pass', t => {
+test('make it pass', (t) => {
   t.truthy(db.Agent, 'Agent services should exist')
 })
 
-test.serial('Setup', t => {
-    t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
-    t.true(AgentStub.hasMany.calledWith(MetricStub), 'Arguments should be the MetricModel')
-    t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
-    t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Arguments should be the AgentModel')
+test.serial('Setup', (t) => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(
+    AgentStub.hasMany.calledWith(MetricStub),
+    'Arguments should be the MetricModel'
+  )
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(
+    MetricStub.belongsTo.calledWith(AgentStub),
+    'Arguments should be the AgentModel'
+  )
+})
+
+test.serial('Agent#findById', async t => {
+  const agent = await db.Agent.findById(id)
+
+  t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
 })
