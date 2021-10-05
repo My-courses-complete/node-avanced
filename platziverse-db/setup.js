5b0a1ed35b524817a6816c8f8e8b3f4b262d1dbf
JMR-Mejia <chucho4720013@gmail.com>
2021-10-05 09:14:35 -0500

    added flag to script setup for confirmation

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -8,20 +8,23 @@ const db = require('./')
 const prompt = inquirer.createPromptModule()
 
 async function setup () {
-  const answer = await prompt([{
-    type: 'confirm',
-    name: 'setup',
-    message: 'This will destroy your database, are you sure?'
-  }])
+  if(process.argv.pop() !== '--yes') {
 
-  if (!answer.setup) {
-    return console.log('Nothing happened :)')
+    const answer = await prompt([{
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }])
+    
+    if (!answer.setup) {
+      return console.log('Nothing happened :)')
+    }
   }
-
+    
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
-    password: process.env.DB_PASS || 'platzifoo',
+    password: process.env.DB_PASS || 'platzi',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: (s) => debug(s),
