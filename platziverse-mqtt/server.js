261e702f8d0ca8b313ab85422eb1aaa7e2b11642
Jesus Mejia <chucho4720013@gmail.com>
2021-10-05 19:07:18 -0500

    Connected MQTT with DB

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,8 +4,20 @@ const debug = require('debug')('platziverse:mqtt')
 const chalk = require('chalk')
 const mqemitter = require('mqemitter-redis')
 const redisPersistence = require('aedes-persistence-redis')
+const db = require('platziverse-db')
 
-function startAedes(){ 
+let Metric, Agent
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'platzi',
+  password: process.env.DB_PASS || 'platzi',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: (s) => debug(s),
+  setup: false
+}
+
+function startAedes () {
   const port = 1883
 
   const aedes = require('aedes')({
@@ -23,9 +35,13 @@ function startAedes(){
 
   const server = require('net').createServer(aedes.handle)
 
-  server.listen(port, function () {
+  server.listen(port, async function () {
+    const services = await db(config).catch(handleFatalError)
+    Metric = services.Metric
+    Agent = services.Agent
+
     debug(`${chalk.green('[platziverse-mqtt]')} server is running`)
-    aedes.publish({topic: 'agent/message', payload: "I'm broker "+aedes.id})
+    aedes.publish({ topic: 'agent/message', payload: "I'm broker " + aedes.id })
   })
 
   aedes.on('client', client => {
@@ -47,9 +63,9 @@ function startAedes(){
 startAedes()
 
 function handleFatalError (err) {
-    console.error(`${chalk.red('[fatal error]')} ${err.message}`)
-    console.error(err.stack)
-    process.exit(1)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
 }
 
 process.on('uncaughtException', handleFatalError)
