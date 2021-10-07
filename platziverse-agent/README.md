a1f2b26eb32953b848bc1ef295306bb48d054f2f
Jesús Mejía <chucho4720013@gmail.com>
2021-10-06 21:43:27 -0500

    Building an-Agent-monitoring (#5)
    * implemented a custom EventEmitter
    
    * Set a timer for executing
    
    * implemented client in MQTT
    
    * Metrics custom
    
    * Example Agent

---
+++ platziverse-agent/README.md
@@ -0,0 +1,39 @@
+# platziverse-agent
+
+## Usage
+
+```js
+const PlatziverseAgent = require('platziverse-agent')
+
+const agent = new PlatziverseAgent({
+  name: 'myapp',
+  username: 'admin',
+  interval: 2000
+})
+
+agent.addMetric('rss', function getRss() {
+  return process.memoryUsafe().rss
+})
+
+agent.addMetric('promiseMetric', function getRandomPromise() {
+  return Promise.resolve(Math.random())
+})
+
+agent.addMetric('callback', function getRandomCallback (callback) {
+  setTimeout(() => { callback(null, Math.random())}, 1000)
+})
+
+agent.connect()
+
+agent.on('connected')
+agent.on('disconnected')
+agent.on('message')
+
+agent.on('agent/connected')
+agent.on('agent/disconnected')
+agent.on('agent/message', payload => {
+  console.log(payload)
+})
+
+setTimeout(() => agent.disconnect(), 20000)
+```
---
+++ platziverse-agent/examples/index.js
@@ -0,0 +1,35 @@
+const PlatziverseAgent = require('../')
+
+const agent = new PlatziverseAgent({
+  name: 'myapp',
+  username: 'admin',
+  interval: 2000
+})
+
+agent.addMetric('rss', function getRss () {
+  return process.memoryUsage().rss
+})
+
+agent.addMetric('promiseMetric', function getRandomPromise () {
+  return Promise.resolve(Math.random())
+})
+
+agent.addMetric('callback', function getRandomCallback (callback) {
+  setTimeout(() => { callback(null, Math.random()) }, 1000)
+})
+
+agent.connect()
+
+agent.on('connect', handler)
+agent.on('disconnected', handler)
+agent.on('message', handler)
+
+agent.on('agent/connected', handler)
+agent.on('agent/disconnected', handler)
+agent.on('agent/message', handler)
+
+function handler (payload) {
+  console.log(payload)
+}
+
+setTimeout(() => agent.disconnect(), 20000)
