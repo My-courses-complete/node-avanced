5e4df057c5d16627588b083878847a08b432dd7c
Jesus Mejia <chucho4720013@gmail.com>
2021-10-06 23:00:19 -0500

    Created routes in API

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -5,9 +5,13 @@ const chalk = require('chalk')
 const http = require('http')
 const express = require('express')
 
+const api = require('./api')
+
 const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
+
+app.use('/api', api)
 
 server.listen(port, () => {
   debug(`listening in ${chalk.green(`http://localhost:${port}`)}`)
