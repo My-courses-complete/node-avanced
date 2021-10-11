266a1e7c244aadfaa5edfd156d985e5293a4d212
Jesus Mejia <chucho4720013@gmail.com>
2021-10-11 14:52:53 -0500

    Integrated socket.io

--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -4,15 +4,29 @@ const debug = require('debug')('platziverse:web')
 const http = require('http')
 const path = require('path')
 const express = require('express')
+const socketio = require('socket.io')
 const chalk = require('chalk')
 
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-function handleFatalError(err) {
+io.on('connect', socket => {
+  debug(`Connected ${socket.id}`)
+
+  socket.on('agent/message', payload => {
+    console.log(payload)
+  })
+
+  setInterval(() => {
+    socket.emit('agent/message', { agent: 'xxx-yyy' })
+  }, 5000)
+})
+
+function handleFatalError (err) {
   console.error(`${chalk.red('[fatl error]')} ${err.message}`)
   console.log(err.stack)
   process.exit(1)
