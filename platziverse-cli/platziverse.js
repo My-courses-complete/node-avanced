fcf068dfdf342761822ed12aa0862fe8ac5ef846
Jesus Mejia <chucho4720013@gmail.com>
2021-10-15 23:07:46 -0500

    Layout Basic with Blessed

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -2,8 +2,32 @@
 
 'use strict'
 
-const minimist = require('minimist')
+/* eslint new-cap: "off" */
 
-console.log('Hello Platziverse!')
-const args = minimist(process.argv)
-console.log(args)
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
+
+const screen = blessed.screen()
+
+const grid = new contrib.grid({
+  rows: 1,
+  cols: 4,
+  screen
+})
+
+const tree = grid.set(0, 0, 1, 1, contrib.tree, {
+  label: 'Connected Agents'
+})
+
+const line = grid.set(0, 1, 1, 3, contrib.line, {
+  label: 'Metric',
+  showLegend: true,
+  minY: 0,
+  xPadding: 5
+})
+
+screen.key(['escape', 'q', 'C-c'], (ch, key) => {
+  process.exit(0)
+})
+
+screen.render()
